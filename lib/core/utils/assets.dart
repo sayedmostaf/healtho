93f071bd4ab995ece08247f7631a7475ad139e0a
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-09-21 03:07:15 +0300

    create sign up screen

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -3,4 +3,6 @@ class AppAssets {
   static const exerciseImage = 'assets/images/exercise_image.png';
   static const healthTrackingImage = 'assets/images/health_tracking_image.png';
   static const progressImage = 'assets/images/progress_image.png';
+  static const phoneImage = 'assets/images/phone.png';
+  static const googleImage = 'assets/images/google.png';
 }
--- lib/features/sign_up/sign_up_page.dart
+++ lib/features/sign_up/sign_up_page.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:healtho/core/utils/assets.dart';
+import 'package:healtho/core/utils/functions/navigation.dart';
+import 'package:healtho/core/widgets/round_button.dart';
+import 'package:healtho/features/splash_page/splash_page.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
 
   @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          children: [
+            const Spacer(
+              flex: 4,
+            ),
+            Image.asset(
+              AppAssets.logo,
+              width: context.width * 0.7,
+            ),
+            const SizedBox(
+              height: 150,
+            ),
+            RoundButton(
+                title: 'Mobile number',
+                type: RoundButtonType.line,
+                isPadding: false,
+                image: AppAssets.phoneImage,
+                onPressed: () {
+                  context.push(const SplashPage());
+                }),
+            const SizedBox(
+              height: 30,
+            ),
+            RoundButton(
+                title: 'Google',
+                type: RoundButtonType.line,
+                isPadding: false,
+                image: AppAssets.googleImage,
+                onPressed: () {}),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
   }
 }
