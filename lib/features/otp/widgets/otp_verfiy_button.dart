6e365bfd0e7b64377bdf3d647d33d0892e3d85fc
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-09-21 04:03:50 +0300

    create name page

--- lib/features/otp/widgets/otp_verfiy_button.dart
+++ lib/features/otp/widgets/otp_verfiy_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:healtho/core/utils/functions/navigation.dart';
 import 'package:healtho/core/widgets/round_button.dart';
-import 'package:healtho/features/splash_page/splash_page.dart';
+import 'package:healtho/features/taking_name/name_page.dart';
 
 class VerifyButton extends StatelessWidget {
   const VerifyButton({super.key});
@@ -11,7 +11,7 @@ class VerifyButton extends StatelessWidget {
     return RoundButton(
       title: 'VERIFY',
       onPressed: () {
-        context.push(const SplashPage());
+        context.push(const NamePage());
       },
     );
   }
---
+++ lib/features/taking_name/name_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:healtho/core/utils/colors.dart';
+import 'package:healtho/core/utils/functions/navigation.dart';
+import 'package:healtho/core/widgets/round_button.dart';
+import 'package:healtho/core/widgets/round_text_field.dart';
+import 'package:healtho/features/splash_page/splash_page.dart';
+
+class NamePage extends StatefulWidget {
+  const NamePage({super.key});
+
+  @override
+  State<NamePage> createState() => _NamePageState();
+}
+
+class _NamePageState extends State<NamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              'Enter your name',
+              style: TextStyle(
+                color: AppColors.primaryText,
+                fontSize: 15,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const RoundTextField(hintText: 'i.e Sayed Mostafa'),
+            const SizedBox(
+              height: 40,
+            ),
+            RoundButton(
+                title: 'NEXT',
+                isPadding: false,
+                onPressed: () {
+                  context.push(const SplashPage());
+                }),
+            const Spacer(),
+          ],
+        ),
+      )),
+    );
+  }
+}
