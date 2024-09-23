e7f7c1d9ece01ea62193d125e4ea5b6e16146b6c
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-09-23 20:13:26 +0300

    create dietician page

--- lib/features/login/physique/physique_page.dart
+++ lib/features/login/physique/physique_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:healtho/core/utils/functions/navigation.dart';
 import 'package:healtho/core/widgets/round_button.dart';
 import 'package:healtho/features/home/notifications/notification_page.dart';
+import 'package:healtho/features/home/reminder/reminder_page.dart';
 import 'package:healtho/features/home/top_tab_view/exercises/exercise_tab_page.dart';
 import 'package:healtho/features/home/top_tab_view/trainer/trainer_tab_page.dart';
 import 'package:healtho/features/login/physique/widgets/age_selector.dart';
@@ -56,7 +57,7 @@ class _PhysiquePageState extends State<PhysiquePage> {
               child: RoundButton(
                 title: 'Confirm',
                 onPressed: () {
-                  context.push(const NotificationPage());
+                  context.push(const ReminderPage());
                 },
                 isPadding: false,
               ),
