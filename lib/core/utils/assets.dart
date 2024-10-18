d5242b1f058ebe60b84f042bdd2c99955c6be6df
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-10-18 19:54:59 +0300

    refactor code

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -3,22 +3,9 @@ class AppAssets {
   static const exerciseImage = 'assets/images/exercise_image.png';
   static const healthTrackingImage = 'assets/images/health_tracking_image.png';
   static const progressImage = 'assets/images/progress_image.png';
-  static const phoneImage = 'assets/images/phone.png';
   static const googleImage = 'assets/images/google.png';
-  static const radioSelectedImage = 'assets/images/radio_select.png';
-  static const radioUnSelectedImage = 'assets/images/radio_unselect.png';
-  static const nextImage = 'assets/images/next.png';
-  static const downImage = 'assets/images/down.png';
-  static const checkTickImage = 'assets/images/check_tick.png';
-  static const unCheckTickImage = 'assets/images/uncheck.png';
-  static const tipOneImage = 'assets/images/home_1.png';
-  static const tipTwoImage = 'assets/images/home_2.png';
-  static const tipThreeImage = 'assets/images/home_3.png';
+
   static const backImage = 'assets/images/back.png';
-  static const favColorImage = 'assets/images/fav_color.png';
-  static const shareImage = 'assets/images/share.png';
-  static const favWhiteImage = 'assets/images/fav_white.png';
-  static const shareWhiteImage = 'assets/images/share_white.png';
   static const chestImage = 'assets/images/chest.png';
   static const backBodyImage = 'assets/images/back_body.png';
   static const bicepsImage = 'assets/images/biceps.png';
@@ -26,67 +13,4 @@ class AppAssets {
   static const shouldersImage = 'assets/images/shoulders.png';
   static const absImage = 'assets/images/abs.png';
   static const legsImage = 'assets/images/legs.png';
-  static const chestTwoImage = 'assets/images/chest_2.png';
-  static const pressOneImage = 'assets/images/press_1.png';
-  static const pressTwoImage = 'assets/images/press_2.png';
-  static const pressThreeImage = 'assets/images/press_3.png';
-  static const pressFourImage = 'assets/images/press_4.png';
-  static const pressFiveImage = 'assets/images/press_5.png';
-  static const downWhiteImage = 'assets/images/down_white.png';
-  static const locationImage = 'assets/images/location.png';
-  static const trainerProfileImage = 'assets/images/trainer_profile.png';
-  static const twiteerImage = 'assets/images/twiter.png';
-  static const colorFacebookImage = 'assets/images/color_facebook.png';
-  static const instaImage = 'assets/images/insta.png';
-  static const youtubeImage = 'assets/images/youtube.png';
-
-  static const trainerOneImage = 'assets/images/trainer_1.png';
-  static const trainerTwoImage = 'assets/images/trainer_2.png';
-  static const trainerThreeImage = 'assets/images/trainer_3.png';
-  static const trainerFourImage = 'assets/images/trainer_4.png';
-  static const trainerFiveImage = 'assets/images/trainer_5.png';
-  static const trainerSixImage = 'assets/images/trainer_6.png';
-  static const trainerSevenImage = 'assets/images/trainer_7.png';
-
-  static const deleteImage = 'assets/images/delete.png';
-  static const addBigImage = 'assets/images/add_big.png';
-  static const challengeOneImage = 'assets/images/challenge_1.png';
-  static const challengeTwoImage = 'assets/images/challenge_2.png';
-  static const challengeThreeImage = 'assets/images/challenge_3.png';
-  static const challengeFourImage = 'assets/images/challenge_4.png';
-  static const challengeFiveImage = 'assets/images/challenge_5.png';
-  static const challengeSixImage = 'assets/images/challenge_6.png';
-  static const challengeSevenImage = 'assets/images/challenge_7.png';
-  static const challengeEightImage = 'assets/images/challenge_8.png';
-
-  static const dataImage = 'assets/images/data.png';
-  static const feedbackImage = 'assets/images/feedback.png';
-  static const languageImage = 'assets/images/language.png';
-  static const logoutImage = 'assets/images/logout.png';
-  static const notificationImage = 'assets/images/notification.png';
-  static const ratingImage = 'assets/images/rating.png';
-  static const userPlaceholderImage = 'assets/images/user_placeholder.png';
-
-  static const smartWatchImage = 'assets/images/smartwatch.png';
-  static const levelImage = 'assets/images/level.png';
-  static const goalImage = 'assets/images/goal.png';
-  static const challengesImage = 'assets/images/challenges.png';
-  static const calenderImage = 'assets/images/calendar.png';
-
-  static const dayOneImage = 'assets/images/day_1.png';
-  static const dayTwoImage = 'assets/images/day_2.png';
-  static const dayThreeImage = 'assets/images/day_3.png';
-  static const dayFourImage = 'assets/images/day_4.png';
-  static const dayFiveImage = 'assets/images/day_5.png';
-
-  static const exerciseOneImage = 'assets/images/ed_1.png';
-  static const exerciseTwoImage = 'assets/images/ed_2.png';
-
-  static const workPlanOneImage = 'assets/images/wp_1.png';
-  static const workPlanTwoImage = 'assets/images/wp_2.png';
-  static const workPlanThreeImage = 'assets/images/wp_3.png';
-  static const workPlanFourImage = 'assets/images/wp_4.png';
-
-  static const searchCircleImage = 'assets/images/search_circle.png';
-  static const completedTasksImage = 'assets/images/completed_tasks.png';
 }
