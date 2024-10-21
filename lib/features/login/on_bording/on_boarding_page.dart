a7e507f2213e020f10d34698c00e5ae774338cf3
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-10-21 17:26:33 +0300

    simple refactor

--- lib/features/login/on_bording/on_boarding_page.dart
+++ lib/features/login/on_bording/on_boarding_page.dart
@@ -40,10 +40,6 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
   @override
   void initState() {
     super.initState();
-    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      statusBarColor: Colors.white,
-      statusBarIconBrightness: Brightness.dark,
-    ));
 
     controller.addListener(() {
       setState(() {
@@ -54,47 +50,41 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
 
   @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: const SystemUiOverlayStyle(
-        statusBarColor: Colors.white,
-        statusBarIconBrightness: Brightness.dark,
-      ),
-      child: Scaffold(
-        appBar: const OnBoardingAppbar(),
-        body: Stack(alignment: Alignment.center, children: [
-          PageView.builder(
-              controller: controller,
-              itemCount: pages.length,
-              itemBuilder: (context, index) {
-                return OnBoardingContent(pageObject: pages[index]);
-              }),
-          SafeArea(
-            child: Column(
-              children: [
-                const Spacer(flex: 5),
-                OnboardingIndicator(pages: pages, selectedPage: selectPage),
-                const Spacer(),
-                RoundButton(
-                  title: "Next",
-                  onPressed: () {
-                    final nextPage = controller.page?.round() ?? 0;
-                    if (nextPage >= 2) {
-                      context.push(const SignUpPage());
-                    } else {
-                      controller.animateToPage(
-                        nextPage + 1,
-                        duration: const Duration(milliseconds: 250),
-                        curve: Curves.easeInOut,
-                      );
-                    }
-                  },
-                ),
-                const SizedBox(height: 20),
-              ],
-            ),
+    return Scaffold(
+      appBar: const OnBoardingAppbar(),
+      body: Stack(alignment: Alignment.center, children: [
+        PageView.builder(
+            controller: controller,
+            itemCount: pages.length,
+            itemBuilder: (context, index) {
+              return OnBoardingContent(pageObject: pages[index]);
+            }),
+        SafeArea(
+          child: Column(
+            children: [
+              const Spacer(flex: 5),
+              OnboardingIndicator(pages: pages, selectedPage: selectPage),
+              const Spacer(),
+              RoundButton(
+                title: "Next",
+                onPressed: () {
+                  final nextPage = controller.page?.round() ?? 0;
+                  if (nextPage >= 2) {
+                    context.push(const SignUpPage());
+                  } else {
+                    controller.animateToPage(
+                      nextPage + 1,
+                      duration: const Duration(milliseconds: 250),
+                      curve: Curves.easeInOut,
+                    );
+                  }
+                },
+              ),
+              const SizedBox(height: 20),
+            ],
           ),
-        ]),
-      ),
+        ),
+      ]),
     );
   }
 }
